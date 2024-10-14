70ac67986517ab1ca21d73b290a9a1e5fae63e22
Andonie <andoniapaco@yahoo.com>
2024-10-14 21:43:06 +0200

    Plan updates

---
+++ TriDViewAPI/DTO/PlanDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TriDViewAPI.DTO
+{
+    public class PlanDTO
+    {
+        public int Id { get; set; }
+        public string PlanName { get; set; }
+
+    }
+}
---
+++ TriDViewAPI/Services/PlanService.cs
@@ -0,0 +1,33 @@
+﻿
+using TriDViewAPI.Data.Repositories.Interfaces;
+using TriDViewAPI.DTO;
+using TriDViewAPI.Models;
+using TriDViewAPI.Services.Interfaces;
+
+namespace TriDViewAPI.Services
+{
+    public class PlanService: IPlanService
+    {
+        private readonly IPlanRepository _planRepository;
+        private readonly ILogService _logService;
+        public PlanService( IPlanRepository planRepository, IConfiguration configuration, ILogService logService)
+        {
+            _planRepository = planRepository;
+            _logService = logService;
+        }
+
+        public async Task<IEnumerable<PlanDTO>> GetAllPlans()
+        {
+            try
+            {
+                return await _planRepository.GetAllPlansAsync();
+            }
+            catch (Exception ex)
+            {
+                _logService.LogError("PlanService", ex.ToString());
+            }
+            return Enumerable.Empty<PlanDTO>();
+            ;
+        }
+    }
+}
